7895ccf0b097e1cec96632ec708efa5c5f6ceff7
MaximMarin2233 <bafldalovdursun78@gmail.com>
2026-02-21 13:59:32 +0700

    add api (account balance), init inf for money page

---
+++ src/app/api/account/balance/route.ts
@@ -0,0 +1,97 @@
+import { NextResponse } from 'next/server'
+import { dbConnections } from '@/lib/db'
+
+export async function GET() {
+  try {
+    const db = dbConnections[1]
+    const charId = 150644
+
+    const [balanceRows]: any = await db.query(
+      `
+        SELECT
+            DATE(date) AS date,
+            SUM(IFNULL(cash_value,0) + IFNULL(bank_value,0)) AS total_amount
+        FROM server1.log_action
+        WHERE date >= DATE_SUB(CURDATE(), INTERVAL 7 DAY)
+        AND char_id = ?
+        GROUP BY DATE(date)
+        ORDER BY date;
+      `,
+      [charId]
+    )
+
+    const [operationsRows]: any = await db.query(
+      `
+        SELECT
+            la.id,
+            la.date,
+            lac.text AS category_name,
+            lar.text AS reason_name,
+            c.char_name AS player_name,
+            la.char_id,
+            tc.char_name AS to_player_name,
+            la.to_char_id,
+            la.vehicle_id,
+            vl.vehicle_name,
+            la.house_id,
+            la.entrance_id,
+            la.flat_id,
+            la.business_id,
+            b.name AS business_name,
+            la.family_id,
+            fam.family_name,
+            la.fraction_id,
+            fra.name AS fraction_name,
+            la.item_id,
+            inv.text AS inventory_name,
+            la.weapon_id,
+            w.text AS weapon_name,
+            la.extra_type,
+            la.extra_id,
+            la.extra_count,
+            la.cash_value,
+            la.cash_after,
+            la.bank_value,
+            la.bank_after,
+            la.donate_value,
+            la.donate_after,
+            la.int_value,
+            la.int_after,
+            la.float_value,
+            la.float_after,
+            la.x,
+            la.y,
+            la.z,
+            la.a,
+            la.world,
+            la.interior,
+            la.text
+        FROM server1.log_action la
+        LEFT JOIN server1.log_action_category lac ON la.category_id = lac.category_id
+        LEFT JOIN server1.log_action_reason lar ON la.reason_id = lar.reason_id
+        LEFT JOIN server1.characters c ON la.char_id = c.char_id
+        LEFT JOIN server1.characters tc ON la.to_char_id = tc.char_id
+        LEFT JOIN server1.vehicle_list vl ON la.model_id = vl.id
+        LEFT JOIN server1.business b ON la.business_id = b.id
+        LEFT JOIN server1.family fam ON la.family_id = fam.id
+        LEFT JOIN server1.fraction fra ON la.fraction_id = fra.id
+        LEFT JOIN server1.log_inventory_items inv ON la.item_id = inv.id
+        LEFT JOIN server1.weapon_name w ON la.weapon_id = w.id
+        WHERE la.char_id = ?
+        AND (la.cash_value IS NOT NULL OR la.bank_value IS NOT NULL)
+        ORDER BY la.date DESC
+        LIMIT 4;
+      `,
+      [charId]
+    )
+
+    return NextResponse.json({
+      balanceWeek: balanceRows || [],
+      lastOperations: operationsRows || []
+    })
+
+  } catch (err: any) {
+    console.error('BALANCE API ERROR:', err)
+    return NextResponse.json({ error: err.message }, { status: 500 })
+  }
+}
